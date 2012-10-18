1a66c71ca5a5b9fedd7e4df5780d6c35d09c9fdd
Benedikt Winder <benedikt.winder@fhv.at>
2012-10-18 15:12:42 +0200

    Admin interface search bar

--- software/admin.py
+++ software/admin.py
@@ -1,4 +1,12 @@
 from software.models import Software
+from software.models import Lector
 from django.contrib import admin
 
-admin.site.register(Software)
+class SoftwareAdmin(admin.ModelAdmin):
+    list_display = ('manufacturer', 'product', 'appv', 'version', 'comment')
+    search_fields = ['product']
+
+#class LectorAdmin(admin.ModelAdmin)
+#    list_display = ('username', 'fullname', 'telephone')
+admin.site.register(Software, SoftwareAdmin)
+admin.site.register(Lector)
