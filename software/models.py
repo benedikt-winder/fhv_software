9f184f7c48ed8f5588314617214f8ff246a058d7
Benedikt Winder <benedikt.winder@fhv.at>
2012-10-18 11:07:40 +0200

    Working choice field

--- software/models.py
+++ software/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Software(models.Model):
     manufacturer = models.CharField(max_length=200)
     product = models.CharField(max_length=200)
-    appv = models.CharField(max_length=200)
+    APPVYES = 'JA'
+    APPVNO = 'NA'
+    CHOICEAPPV = ((APPVYES, 'Yes'), (APPVNO, 'No'),)
+    appv = models.CharField(max_length=5, choices=CHOICEAPPV, default=APPVYES)
     version = models.CharField(max_length=200)
     comment = models.CharField(max_length=200)
     def __unicode__(self):
@@ -11,5 +14,7 @@ class Software(models.Model):
 
 class Lector(models.Model):
     username = models.CharField(max_length=5)
+    fullname = models.CharField(max_length=100)
+    telephone = models.IntegerField()
     def __unicode__(self):
         return self.username
