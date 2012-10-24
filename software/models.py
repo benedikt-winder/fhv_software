a048466663ed99d96d2d71af7141f7e6b7b36ae3
Benedikt Winder <benedikt.winder@fhv.at>
2012-10-24 08:41:12 +0200

    Added first view

--- software/models.py
+++ software/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Software(models.Model):
+    sw_id = models.AutoField(primary_key=True)
     manufacturer = models.CharField(max_length=200)
     product = models.CharField(max_length=200)
     APPVYES = 'JA'
@@ -10,13 +11,14 @@ class Software(models.Model):
     version = models.CharField(max_length=200)
     comment = models.CharField(max_length=200)
     def __unicode__(self):
-        return self.manufacturer
+        return '%s %s (%s)' %(self.manufacturer, self.product, self.version)
 
 class Lector(models.Model):
+    lector_id = models.AutoField(primary_key=True)
+    software = models.ForeignKey(Software)
     username = models.CharField(max_length=5)
     fullname = models.CharField(max_length=100)
     telephone = models.IntegerField()
     def __unicode__(self):
-        return self.username
-    
-# Test
+        return '%s (%s)' %(self.fullname, self.username)
+
--- software/views.py
+++ software/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from models import *
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def software_byname(request):
+    print request.GET
+    objects = Lector.objects.filter(username = 'bw')
+    params = {
+        'objects':objects
+        }
+        
+    return render_to_response('software_byname.html', params, context_instance=RequestContext(request))
